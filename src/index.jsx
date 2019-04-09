20c90655602768b381cc613bb2ccc52579d6ec70
boksul <boksul@hotmail.com>
2019-04-09 01:50:20 +0000

    built boilerplate

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,4 @@
-// Application entrypoint.
-
-// Load up the application styles
 require("../styles/application.scss");
-
-// Render the top-level React component
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
